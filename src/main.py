9d921d2cc725465ec51106b2845c1f8d161c97c6
Antonio Velázquez <43018998+antoniomx1@users.noreply.github.com>
2026-01-16 10:45:13 -0600

    Create logs directory if it doesn't exist
    Add log directory creation if it does not exist

--- src/main.py
+++ src/main.py
@@ -8,6 +8,12 @@ from load import load_data_to_postgres
 # Carga variables de entorno (para local y para que lea los secretos)
 load_dotenv()
 
+# Crear carpeta de logs si no existe 
+if not os.path.exists('logs'):
+    os.makedirs('logs')
+
+
+
 # Configuración de logs
 logging.basicConfig(
     level=logging.INFO,
